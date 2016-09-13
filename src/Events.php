9b269f34ac619e76f14828f56352d3178cf6d70c
Petr Buchin <petrbuchyn@gmail.com>
2016-09-14 00:51:30 +0300

    Removed event names, they will be added in future after Connection and Collection API will be stable

--- src/Events.php
+++ src/Events.php
@@ -4,12 +4,6 @@ namespace Tequilla\MongoDB;
 
 class Events
 {
-    const BEFORE_DATABASE_SELECTED = 'tequilla.mongodb.before.database.selected';
-    const DATABASE_SELECTED = 'tequilla.mongodb.database.selected';
-    const BEFORE_DATABASE_DROPPED = 'tequilla.mongodb.before.database.dropped';
-    const DATABASE_DROPPED = 'tequilla.mongodb.database.dropped';
     const BEFORE_DATABASE_COMMAND_EXECUTED = 'tequilla.mongodb.before.database.command.executed';
     const DATABASE_COMMAND_EXECUTED = 'tequilla.mongodb.database.command.executed';
-    const INDEXES_CREATED = 'tequilla.mongodb.indexes.created';
-    const INDEXES_DROPPED = 'tequilla.mongodb.indexes.dropped';
 }
